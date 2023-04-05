558fec716b2143dd0ae045d20da27c487d48f769
uuuuu64160167 <64160167@go.buu.ac.th>
2023-04-05 16:32:15 +0700

    หน้า Show History
    คำอธิบาย : จัดหน้าให้ตรงม็อคอัพ
    ผู้รับผิดชอบ : ไอซ์
    วันที่ 05 / 04 / 2566

--- resources/views/v_show_history.blade.php
+++ resources/views/v_show_history.blade.php
@@ -6,75 +6,94 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-lg-13">
+
+            <!-- กรอบพื้นหลัง -->
             <div class="card">
-                <div class="card-header fs-4 py-3">{{ __('เบิกสวัสดิการแบบรายบุคคล') }}</div>
-                <div class="card-body">
+
+                <!-- หัวข้อใบเบิก -->
+                <div class="bold_header mx-4" style="line-height:20px">{{ __('เบิกสวัสดิการแบบบุคคล') }}
+                    <hr width="295" class="mb-2">{{-- เส้นใต้ --}}
+                </div>
+
+                <div class="card-body mx-5 px-5 mb-0 border-0 p-0">
+
+                    <!-- กรอบข้อมูล -->
                     <form method="POST" action="{{ route('create.single') }}" enctype="multipart/form-data">
                         @csrf
                         <div class="row mt-2">
+                            <div class="card px-4 py-3 pb-4 border-0" style=" background-color: #eee;">
+                                <div class="d-flex justify-content-end">
+                                    <div class="row text-right mt-2">
 
-                        </div>
+                                        <!-- วันที่ -->
+                                        <label for="budget" class="col-auto col-form-label ms-auto fw-bold ">{{ __('วัน/เดือน/ปี : ') }}</label>
+                                        <div class="col-sm-3 ">
+                                            <input type="text" id="date" name="date" class="form-control border-0 bg-transparent " value="{{ date("d/m/Y", strtotime($history->create_date)) }}" disabled>
+                                        </div>
 
-                        <div class="row mt-2">
-                            <label for="req-id" class="col-auto col-form-label ms-auto">{{ __('เลขที่ใบเบิก : ') }}</label>
-                                <div class="col-sm-2">
-                                    <input type="text" name="req-id" id="req-id" class="form-control" value="{{ $history->id }}" disabled>
+                                        <!-- เลขที่ใบเบิก -->
+                                        <label for="req-id" class="col-auto col-form-label fw-bold">{{ __('เลขที่ใบเบิก : ') }}</label>
+                                        <div class="col-sm-2 ">
+                                            <input type="text" name="req-id" id="req-id" class="form-control border-0 bg-transparent top-0 end-0" value="{{ $history->id }}" disabled>
+                                        </div>
+                                        </label>
+                                    </div>
                                 </div>
-                            </label>
 
-                            <label for="budget" class="col-auto col-form-label">{{ __('วัน/เดือน/ปี : ') }}</label>
-                            <div class="col-sm-2">
-                                <input type="text" id="date" name="date" class="form-control" value="{{ date("d/m/Y", strtotime($history->create_date)) }}" disabled>
+                                <!-- รหัสพนักงาน -->
+                                <div class="row">
+                                    <label for="id" class="col-sm-2 col-form-label fw-bold">{{ __('รหัสพนักงาน : ') }}</label>
+                                    <div class="col-sm-3">
+                                        <input type="text" class="form-control border-0 bg-transparent " value="{{ $history->get_user->id }}" disabled>
+                                    </div>
+                                </div>
+
+                                <!-- ชื่อ-นามกสุล -->
+                                <div class="row">
+                                    <label for="name" class="col-sm-2 col-form-label fw-bold">{{ __('ชื่อ-สกุล : ') }}</label>
+                                    <div class="col-md-3">
+                                        <input type="text" class="form-control border-0 bg-transparent " value="{{ $history->get_user->fname }} {{ $history->get_user->lname }}" disabled>
+                                    </div>
+                                </div>
+
+                                <!-- แผนก -->
+                                <div class="row">
+                                    <label for="department" class="col-sm-2 col-form-label fw-bold">{{ __('แผนก : ') }}</label>
+                                    <div class="col-sm-3">
+                                        <input type="text" class="form-control border-0 bg-transparent " value="{{ $history->get_user->department->name }}" disabled>
+                                </div>
                             </div>
                         </div>
-
-                        <div class="row mt-4">
-                            <label for="id" class="col-sm-2 col-form-label">{{ __('รหัสพนักงาน : ') }}</label>
-                            <div class="col-sm-3">
-                                <input type="text" class="form-control" value="{{ Auth::user()->id }}" disabled>
-                            </div>
-
-                        </div>
-
-                        <div class="row mt-4">
-                            <label for="name" class="col-sm-2 col-form-label">{{ __('ชื่อ-สกุล : ') }}</label>
-                            <div class="col-md-3">
-                                <input type="text" class="form-control" value="{{ Auth::user()->fname }} {{ Auth::user()->lname }}" disabled>
-                            </div>
-                        </div>
-
-                        <div class="row mt-4">
-                            <label for="department" class="col-sm-2 col-form-label">{{ __('แผนก : ') }}</label>
-                            <div class="col-sm-3">
-                                <input type="text" class="form-control" value="{{ Auth::user()->department->name }}" disabled>
-                            </div>
                         </div>
 
                         <div class="row mt-5">
+
+                            <!-- แสดงประเภทสวัสดิการ -->
                             <label for="welfare" class="col-auto col-form-label">{{ __('ประเภทสวัสดิการ : ') }}</label>
                             <div class="col-md-5">
-                                <input type="text" class="form-control" value="{{ $history->get_welfare->title }}" name="" id="" readonly       >
+                                <input type="text" class="form-control bg-transparent border-dark" value="{{ $history->get_welfare->title }}" name="" id="" readonly >
                             </div>
 
+                            <!-- แสดงจำนวนเงินที่เบิกได้ -->
                             <label for="budget" class="col-auto col-form-label ms-auto">{{ __('จำนวนเงินที่เบิกได้ : ') }}</label>
                             <div class="col-sm-2">
-                                <input type="text" class="text-end form-control" value="{{ $history->get_welfare->budget }}" disabled>
+                                <input type="text" class="text-end form-control border-0" value="{{ __('2,000.00') }}" disabled>
                             </div>
 
                             <label for="id" class="col-auto col-form-label">{{ __('บาท') }}</label>
                         </div>
 
                         <div class="row mt-5 fs-5">
-                            <div class="col-md-4">
+                            <div class="col-md-4 mb-0">
                                 รายละเอียดการขอเบิกสวัสดิการ
-                                <hr>
+                                <hr width="260" class="mb-0">
                             </div>
-                            </div>
+                        </div>
 
                         <div class="row mt-3 d-flex justify-content-center">
-                            <div class="col-lg-11">
-                                <table id="detail" class="table table-bordered">
-                                    <thead>
+                            <div class="col-lg-20">
+                                <table id="detail" class="table table-bordered border-dark" style="left: 20px">
+                                    <thead id="bg" class="text-white">
                                         <tr class="text-center">
                                             <th scope="col" class="col-lg-8">รายละเอียด</th>
                                             <th scope="col" class="">จำนวนเงิน (บาท)</th>
@@ -83,12 +102,10 @@
 
                                     <tbody>
                                         @php ($price = json_decode($history->price))
-                                        @php ($price_total = 0)
                                         @foreach (json_decode($history->item) as $index => $item)
-                                            <!-- {{$price_total = $price[$index]}};-->
                                             <tr>
-                                                <td><input type="text" class="form-control" value="{{ $item }}" readonly></td>
-                                                <td><input type="text" class="form-control text-end" value="{{ number_format($price[$index],2) }}" readonly></td>
+                                                <td><input type="text" class="form-control border-0" value="{{ $item }}" readonly></td>
+                                                <td><input type="text" class="form-control text-end border-0" value="{{ $price[$index] }}" readonly></td>
                                             </tr>
                                         @endforeach
                                     </tbody>
@@ -97,28 +114,31 @@
                         </div>
 
                         <div class="row mt-3">
-                            <label for="total" class="col-auto col-form-label">จำนวนเงินทั้งหมด : </label>
+                            <label for="total" class="col-auto col-form-label ms-auto">จำนวนเงินทั้งหมด : </label>
                             <div class="col-sm-2">
-                                <input type="text" class="form-control text-end" value="{{ number_format($price_total,2) }}" readonly>
+                                <input type="text" class="form-control text-end border-0" style=" background-color: #eee;" value="{{ __('0') }}" readonly>
                             </div>
                             <label for="" class="col-auto col-form-label">{{ __('บาท') }}</label>
                         </div>
 
-                        <div class="row mt-3">
-                            <label for="bill" class="col-auto col-form-label">ไฟล์ใบเสร็จ : </label>
-                                <div class="col-sm-5">
-                                    @foreach (json_decode($history->bill) as $bill)
-                                        <div class="row mt-3">
-                                            <a href="" for="" class="col-auto col-form-label">{{ $bill }}</a>
-                                        </div>
-                                    @endforeach
+
+                        <div class="row mt-4">
+                            <div class="col-md-6">
+                                <label for="bill">ไฟล์หลักฐาน</label>
+                                    <div class="card-text px-4 rounded me-3" style="background-color: #eee">
+                                        @foreach (json_decode($history->bill) as $bill)
+                                            <div class="row">
+                                                <a href="{{ URL::asset('/bills/'. $bill) }}" for="" class="col-auto col-form-label" target="blank">{{ $bill }}</a>
+                                            </div>
+                                        @endforeach
+                                    </div>
                                 </div>
                         </div>
 
                         <div class="row mt-3">
                             @if (($history->status) == 0)
                             <div class="col-sm-2 ms-auto">
-                                <a href="{{ url('cancel/'. $history->id) }}" method="POST" class="btn btn-lg btn-danger">ยกเลิก</a>
+                                <a href="{{ url('cancel/'. $history->id) }}" method="POST" class="btn btn-lg btn-danger mb-5">ยกเลิก</a>
                             </div>
                             @endif
                         </div>
