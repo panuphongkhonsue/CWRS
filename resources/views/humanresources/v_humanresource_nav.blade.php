7774b2bfcc707e659c117913d2ee17efea5fec00
phurin284 <64160284@go.buu.ac.th>
2023-04-06 16:42:56 +0700

    หน้าจอรายการคำขออนุมัติ
    คำอฺธิบาย : จัดทำหน้าจอรายการคำขออนุมัติเสร็จ
    ผู้รับผิดชอบ : ภูรินทร์
    วันที่ : 05/04/2566

--- resources/views/humanresources/v_humanresource_nav.blade.php
+++ resources/views/humanresources/v_humanresource_nav.blade.php
@@ -18,6 +18,8 @@
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ URL::asset('/css/home.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('/css/ohm/phurin.css') }}">
+
     @livewireStyles
 </head>
 <body>
--- resources/views/livewire/request-filter.blade.php
+++ resources/views/livewire/request-filter.blade.php
@@ -1,10 +1,16 @@
 <div>
     <div class="row">
         <div class="col-md-3">
-            <div class="card">
+            <div class="card ">
                 <button wire:click="filter({{ 999 }})" type="button" class="btn btn-custom" value="999">
                     <div class="card-body">
-                        <div class="text-center text-primary">ทั้งหมด</div>
+                        <div class="row justify-content-center">
+                            <div class="col text-start ms-3"><img  class="" src="{{ URL::asset('img/image.png') }}" width="80" height="80"></div>
+                            <div class="col text-center me-5 mt-2">
+                                <div class="text-primary fs-5">ทั้งหมด</div>
+                                <div class="fs-5">219</div>
+                            </div>
+                        </div>
                     </div>
                 </button>
             </div>
@@ -14,7 +20,16 @@
             <div class="card">
                 <button wire:click="filter({{ 0 }})" type="button" id="selected" class="btn btn-custom" value="0">
                     <div class="card-body">
-                        <div class="text-center text-warning">รออนุมัติ</div>
+                        <div class="row justify-content-center">
+                            <div class="col text-start ms-3"><img  class="" src="{{ URL::asset('img/pending-approval.png') }}" width="80" height="80"></div>
+                            <div class="col text-center me-5 mt-2">
+                                <div class="text-warning fs-5">รออนุมัติ</div>
+                                <div class="fs-5">219</div>
+                            </div>
+                        </div>
+
+
+
                     </div>
                 </button>
             </div>
@@ -24,7 +39,16 @@
             <div class="card">
                 <button wire:click="filter({{ 1 }})" type="button" class="btn btn-custom" value="1">
                     <div class="card-body">
-                        <div class="text-center text-success">อนุมัติ</div>
+
+
+                        <div class="row justify-content-center">
+                            <div class="col text-start ms-3"><img  class="" src="{{ URL::asset('img/อนุมัติ.png') }}" width="80" height="80"></div>
+                            <div class="col text-center me-5 mt-2">
+                                <div class="text-success fs-5">อนุมัติ</div>
+                                <div class="fs-5">219</div>
+                            </div>
+                        </div>
+
                     </div>
                 </button>
             </div>
@@ -34,10 +58,19 @@
             <div class="card">
                 <button wire:click="filter({{ -2 }})" type="button" class="btn btn-custom" value="-1">
                     <div class="card-body">
-                        <div class="text-center text-danger">ไม่อนุมัติ</div>
+
+                        <div class="row justify-content-center">
+                            <div class="col text-start ms-3"><img  class="" src="{{ URL::asset('img/ไม่อนุมัติ.png') }}" width="80" height="80"></div>
+                            <div class="col text-center me-5 mt-2">
+                                <div class="text-danger fs-5">ไม่อนุมัติ</div>
+                                <div class="fs-5">219</div>
+                            </div>
+                        </div>
+
                     </div>
                 </button>
             </div>
         </div>
     </div>
 </div>
+
