fa090bce4ebb039fcd29bcf44c7aa80a58856424
bench165 <118896425+bench165@users.noreply.github.com>
2023-04-06 16:03:53 +0700

    ล็อกอิน
    อธิบาย : จัดตัวแจ้งเตือน
    ผู้จัดทำ : เบญจมาภรณ์
    วันที่ : 06/04/2566

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,67 +7,62 @@
     <div class="container">
         <div class="card-login bot">
             <img src="/img/clicknext.png" alt="" class="img-login">
-
             <div class="card-body">
-                    {{--  <div class="card bg-info" style="padding-top: 180px">
-                            <div class="card-body bg-danger col-md-7 align-self-center"> --}}
-                                <form method="POST" action="{{ route('login') }}">
-                                    @csrf
-                                    <div class="row">
-                                        @if(Session::has('message'))
-                                            <div class="alert alert-danger">
-                                                {{ Session::get('message') }}
-                                            </div>
-                                        @endif
-                                    </div>
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
+                        <!-- กล่องข้อความสำหรับกรอก Username -->
+                    <div class="row">
+                        <div class="col-md-2 input-group mb-3">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text" id="basic-addon1"><img src="{{ URL::asset('img/user.png') }}" class="img-rounded" alt="" width="26" height="26"></span>
+                            </div>
+                                <input id="id" type="text" class="form-control @error('id') is-invalid @enderror" name="id" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="Username">
 
-                                        <!-- กล่องข้อความสำหรับกรอก Username -->
-                                    <div class="row">
-                                        <div class="col-md-2 input-group mb-3">
-                                                <div class="input-group-prepend">
-                                                <span class="input-group-text" id="basic-addon1"><img src="{{ URL::asset('img/user.png') }}" class="img-rounded" alt="" width="26" height="26"></span>
-                                            </div>
-                                            <input id="id" type="text" class="form-control @error('id') is-invalid @enderror" name="id" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="Username">
+                            @error('id')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                    </div>
 
-                                            @error('id')
-                                                <span class="invalid-feedback" role="alert">
-                                                    <strong>{{ $message }}</strong>
-                                                </span>
-                                            @enderror
-                                        </div>
-                                    </div>
+                        <!-- กล่องข้อความสำหรับกรอก Password -->
+                    <div class="row">
+                        <div class="col input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text" id="basic-addon1"><img src="{{ URL::asset('img/lock.png') }}" class="img-rounded" alt="" width="26" height="26"></span>
+                            </div>
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Password">
 
-                                        <!-- กล่องข้อความสำหรับกรอก Password -->
-                                    <div class="row mb-4">
-                                        <div class="col input-group mb-3">
-                                            <div class="input-group-prepend">
-                                                <span class="input-group-text" id="basic-addon1"><img src="{{ URL::asset('img/lock.png') }}" class="img-rounded" alt="" width="26" height="26"></span>
-                                            </div>
-                                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Password">
+                            @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                    </div>
 
-                                            @error('password')
-                                                <span class="invalid-feedback" role="alert">
-                                                    <strong>{{ $message }}</strong>
-                                                </span>
-                                            @enderror
-                                        </div>
-                                    </div>
+                    {{-- แจ้งเตือน --}}
+                    <div class="row">
+                        @if(Session::has('message'))
+                            <div class="alert text-danger">
+                                {{ Session::get('message') }}
+                            </div>
+                        @endif
+                    </div>
 
-                                        <!-- ปุ่ม Sign in -->
-                                    <div class="row mb-0">
-                                        <div class="col text-center">
-                                            <button type="submit" class="btn btn-success">
-                                                {{ __('Sign in  ') }}
-                                            </button>
-                                        </div>
-
-                                    </div>
-                                </form>
+                        <!-- ปุ่ม Sign in -->
+                    <div class="row mb-0">
+                        <div class="col text-center">
+                            <button type="submit" class="btn btn-success" style="position: absolute; top: 80%; left: calc(50% - 35px)">
+                                {{ __('Sign in  ') }}
+                            </button>
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
-
-
-    </div>
 </div>
 @endsection
+
