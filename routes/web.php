e6d469b52fe87a2b198238f8757d4ff562ebce7f
64160299 <64160299@go.buu.ac.th>
2023-04-07 15:44:24 +0700

    3

--- routes/web.php
+++ routes/web.php
@@ -46,7 +46,7 @@ Route::group(['middleware' => 'auth'], function()
    Route::group(['middleware' => 'user:H'], function()
    {
         Route::get('/manage_welfare', [Welfare_controller::class, 'index'])->name('manage_welfare');
-        Route::get('/manage/request', [Manage_request_controller::class, 'index'])->name('manage_request');
+        Route::get('/manage_request', [Manage_request_controller::class, 'index'])->name('manage_request');
         Route::post('/add_welfare', [Welfare_controller::class, 'add_welfare'])->name('add_welfare');
         Route::post('/manage_welfare/edit', [Welfare_controller::class, 'edit_welfare'])->name('edit_welfare');
         Route::get('/report', [Report_controller::class, 'report'])->name('report');
@@ -54,16 +54,15 @@ Route::group(['middleware' => 'auth'], function()
 
    Route::group(['middleware' => 'emplead'], function()
    {
-        Route::get('/request/single', [Request_controller::class, 'single_request'])->name('s.request');
+        Route::get('/request_single', [Request_controller::class, 'single_request'])->name('s.request');
         Route::post('/request/single/create', [Request_controller::class, 'create_single'])->name('create.single');
-        Route::get('/request/group', [Request_controller::class, 'group_request'])->name('group_request');
+        Route::get('/request_group', [Request_controller::class, 'group_request'])->name('group_request');
         Route::get('/history', [User_history_controller::class, 'index'])->name('history');
         Route::get('/history/{id}', [User_history_controller::class, 'show_request'])->name('show_history');
         Route::patch('/history/{id}/cancel', [User_history_controller::class, 'cancel'])->name('cancel');
         Route::get('/history', [User_history_controller::class, 'index'])->name('history');
         Route::post('/request/single/create', [Request_controller::class, 'create_single'])->name('create.single');
         Route::get('/history/{id}', [User_history_controller::class, 'show_request'])->name('show_history');
-        Route::get('/request/group', [Request_controller::class, 'group_request'])->name('group_request');
         Route::post('/request/group/create', [Request_controller::class, 'create_group'])->name('create.group');
    });
 
