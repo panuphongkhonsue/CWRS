95d3954b7f0c5aad864175c10991f2d7784dfb74
64160299 <64160299@go.buu.ac.th>
2023-04-07 15:09:39 +0700

    push

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -25,7 +25,7 @@
                                     <div class="row">
                                         <div class="col-md-2 input-group mb-3">
                                                 <div class="input-group-prepend">
-                                                <span class="input-group-text" id="basic-addon1"><img src="{{ asset('public/img/user.png') }}" class="img-rounded" alt="" width="26" height="26"></span>
+                                                <span class="input-group-text" id="basic-addon1"><img src="./img/user.png" class="img-rounded" alt="" width="26" height="26"></span>
                                             </div>
                                             <input id="id" type="text" class="form-control @error('id') is-invalid @enderror" name="id" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="Username">
 
@@ -41,7 +41,7 @@
                                     <div class="row mb-4">
                                         <div class="col input-group mb-3">
                                             <div class="input-group-prepend">
-                                                <span class="input-group-text" id="basic-addon1"><img src="{{ asset('./img/lock.png') }}" class="img-rounded" alt="" width="26" height="26"></span>
+                                                <span class="input-group-text" id="basic-addon1"><img src="./img/lock.png" class="img-rounded" alt="" width="26" height="26"></span>
                                             </div>
                                             <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Password">
 
