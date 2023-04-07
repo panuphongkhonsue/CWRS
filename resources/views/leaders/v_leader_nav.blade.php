a15d71cb0f5745b3f30c794c6e81a90ee15a5b74
64160299 <64160299@go.buu.ac.th>
2023-04-07 15:50:28 +0700

    d

--- resources/views/leaders/v_leader_nav.blade.php
+++ resources/views/leaders/v_leader_nav.blade.php
@@ -119,8 +119,8 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="//code.jquery.com/jquery-1.12.4.js"></script>
     <script src="//code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-    <link href="{{ asset('dist/css/select2.min.css') }}" rel="stylesheet" />
-    <script src="{{ asset('dist/js/select2.min.js') }}"></script>
+    <link href="./dist/css/select2.min.css" rel="stylesheet" />
+    <script src="./dist/js/select2.min.js"></script>
     <script>
     function openNav() {
       document.getElementById("side_bar").style.left = "0px";
