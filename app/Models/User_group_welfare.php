08e6bc572f74a117a215e419c0311b155b5b6898
C3_64160282_ภานุพศ์ คนซื่อ <64160282@go.buu.ac.th>
2023-04-09 14:12:24 +0700

    commit

---
+++ app/Models/User_group_welfare.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class User_group_welfare extends Model
+{
+    use HasFactory;
+
+    protected $table = 'users_groups_welfares';
+    public $timestamps = false;
+}
