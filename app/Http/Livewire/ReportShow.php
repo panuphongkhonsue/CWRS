04ae3bf203510cfb576002206719edff355a48f9
64160299 <64160299@go.buu.ac.th>
2023-04-09 09:02:10 +0700

    gg

--- app/Http/Livewire/ReportShow.php
+++ app/Http/Livewire/ReportShow.php
@@ -11,6 +11,7 @@ use App\Models\Welfare;
 class ReportShow extends Component
 {
     use WithPagination;
+    public $tb = 1;
     public $report_status = 1;
     public $report_type = "S";
     public $report_year = 999;
@@ -19,6 +20,8 @@ class ReportShow extends Component
     public $year3;
     public $year4;
     public $year5;
+
+    public $month;
 
     public $sum;
 
@@ -47,15 +50,6 @@ class ReportShow extends Component
         $this->year4 = $this->requests->groupBy('welfare_id')->selectRaw('sum(welfare_budget) as sum')->whereYear('create_date', 2020)->get();
         $this->year5 = $this->requests->groupBy('welfare_id')->selectRaw('sum(welfare_budget) as sum')->whereYear('create_date', 2019)->get();
 
-        function check($array, $key)
-        {
-            if (isset($array[$key])) {
-                if (is_null($array[$key])) {
-                    return false;
-                }
-            }
-            return true;
-        }
         $this->requests = 1;
     }
 
@@ -70,5 +64,55 @@ class ReportShow extends Component
         $this->report_status = $report_status;
         $this->report_type = $report_type;
         $this->report_year = $report_year;
+
+        if ($report_year == 999)
+        {
+            $this->tb = 1;
+            $this->welfares = Welfare::where('type', 'S')->get();
+
+            foreach ($this->welfares as $index => $welfare) {
+                $welId[$index] = $welfare->id;
+                $this->count_wel++;
+            }
+
+            $this->requests = Single_request::where('status', 1);
+            $this->requests = $this->requests->whereNotIn('id', $welId);
+
+            $this->year1 = $this->requests->groupBy('welfare_id')->selectRaw('sum(welfare_budget) as sum')->whereYear('create_date', 2023)->get();
+
+            $this->year2 = $this->requests->groupBy('welfare_id')->selectRaw('sum(welfare_budget) as sum')->whereYear('create_date', 2022)->get();
+            $this->year3 = $this->requests->groupBy('welfare_id')->selectRaw('sum(welfare_budget) as sum')->whereYear('create_date', 2021)->get();
+            $this->year4 = $this->requests->groupBy('welfare_id')->selectRaw('sum(welfare_budget) as sum')->whereYear('create_date', 2020)->get();
+            $this->year5 = $this->requests->groupBy('welfare_id')->selectRaw('sum(welfare_budget) as sum')->whereYear('create_date', 2019)->get();
+
+        }
+
+
+        else if ($report_year != 999)
+        {
+            $this->tb = 2;
+            $this->welfares = Welfare::where('type', 'S')->get();
+
+            foreach ($this->welfares as $index => $welfare) {
+                $welId[$index] = $welfare->id;
+                $this->count_wel++;
+            }
+
+            $this->requests = Single_request::where('status', 1);
+            $this->requests = $this->requests->whereNotIn('id', $welId);
+            $this->requests = $this->requests->whereYear('create_date', $report_year)->get();
+
+            for ($i = 0 ; $i < 12 ; $i++) {
+                $this->month[$i] = $this->requests->groupBy('welfare_id')->selectRaw('sum(welfare_budget) as sum')->whereMonth('create_date', $i+1)->get();
+                return dd()
+            }
+
+            $this->year2 = $this->requests->groupBy('welfare_id')->selectRaw('sum(welfare_budget) as sum')->whereYear('create_date', 2022)->get();
+            $this->year3 = $this->requests->groupBy('welfare_id')->selectRaw('sum(welfare_budget) as sum')->whereYear('create_date', 2021)->get();
+            $this->year4 = $this->requests->groupBy('welfare_id')->selectRaw('sum(welfare_budget) as sum')->whereYear('create_date', 2020)->get();
+            $this->year5 = $this->requests->groupBy('welfare_id')->selectRaw('sum(welfare_budget) as sum')->whereYear('create_date', 2019)->get();
+
+            $this->requests = 1;
+        }
     }
 }
--- resources/views/livewire/report-show.blade.php
+++ resources/views/livewire/report-show.blade.php
@@ -1,92 +1,121 @@
 <div>
-    {{-- ในส่วนของตาราง --}}
+    @if ($tb == 1)
 
-<table class="table table-bordered align-items-center" >
-    <thead class="text-center text-light " id="bg" >
-        <tr>
-            <th rowspan="2" class="col-sm-2" valign="middle" align="center">ประเภทสวัสดิการ</th>
-            <th colspan="5" class="col-sm-1">ปี พ.ศ.</th>
-            <th rowspan="2" class="col-sm-2" valign="middle" align="center">จำนวนเงิน (บาท)</th>
-        </tr>
-        <tr class="text-center text-light " id="bg">
-            <td class="col-sm-1">2566</td>
-            <td class="col-sm-1">2565</td>
-            <td class="col-sm-1">2564</td>
-            <td class="col-sm-1">2563</td>
-            <td class="col-sm-1">2562</td></tr>
-        </thead>
-    <tbody>
-        <style >
-            tr:nth-child(even) {
-            background-color: #DCDCDC;
-            }
-        </style >
-        @php
-        function check($array, $key)
-        {
-            if (isset($array[$key])) {
-                if (is_null($array[$key])) {
-                    return false;
-                }
-                return true;
-            }
-        }
-        @endphp
-        @foreach ($welfares as $index => $welfare)
-                <tr>
+        {{-- ในส่วนของตาราง --}}
 
-                        @switch($welfare->type)
-                        @case('S')
-                            @php ($text = 'บุคคล')
-                            @break
-                        @case('G')
-                            @php ($text = 'สันทนาการ')
-                            @break
-                        @endswitch
-
-                            <td class="text-center">{{ $welfare->title }}</td>
-                            @if (isset($year1[$index]))
-                                <td class="text-end">{{ $year1[$index]->sum }}</td>
-                            @else
-                                <td class="text-end">0</td>
-                            @endif
-                           <td class="text-end">0</td>
-                           <td class="text-end">0</td>
-                           <td class="text-end">0</td>
-                           <td class="text-end">0</td>
-                           <td class="text-end">0</td>
-
+    <table class="table table-bordered align-items-center" >
+        <thead class="text-center text-light " id="bg" >
+            <tr>
+                <th rowspan="2" class="col-sm-2" valign="middle" align="center">ประเภทสวัสดิการ</th>
+                <th colspan="5" class="col-sm-1">ปี พ.ศ.</th>
+                <th rowspan="2" class="col-sm-2" valign="middle" align="center">จำนวนเงิน (บาท)</th>
             </tr>
-            @endforeach
-    </tbody>
- </table>
-
-<div class="row mt-3">
-    <div class="col-md-13">
-        <table class="table table-bordered align-items-center" >
-            <thead class="text-center text-light " id="bg" >
-                <tr>
-                    <th rowspan="2" class="col-sm-1" valign="middle" align="center">ประเภทสวัสดิการ</th>
-                    <th colspan="12" class="col-sm-1">เดือน</th>
-                    <th rowspan="2" class="col-sm-2" valign="middle" align="center">จำนวนเงิน (บาท)</th>
-                </tr>
-                <tr class="text-center text-light " id="bg">
-                    <td class="col-sm-1">ม.ค.</td>
-                    <td class="col-sm-1">ก.พ.</td>
-                    <td class="col-sm-1">มี.ค.</td>
-                    <td class="col-sm-1">เม.ย.</td>
-                    <td class="col-sm-1">พ.ค.</td>
-                    <td class="col-sm-1">มิ.ย.</td>
-                    <td class="col-sm-1">ก.ค.</td>
-                    <td class="col-sm-1">ส.ค.</td>
-                    <td class="col-sm-1">ก.ย.</td>
-                    <td class="col-sm-1">ต.ค.</td>
-                    <td class="col-sm-1">พ.ย.</td>
-                    <td class="col-sm-1">ธ.ค.</td>
-                </tr>
+            <tr class="text-center text-light " id="bg">
+                <td class="col-sm-1">2566</td>
+                <td class="col-sm-1">2565</td>
+                <td class="col-sm-1">2564</td>
+                <td class="col-sm-1">2563</td>
+                <td class="col-sm-1">2562</td></tr>
             </thead>
-        </table>
-    </div>
-</div>
+        <tbody>
+            <style >
+                tr:nth-child(even) {
+                background-color: #DCDCDC;
+                }
+            </style >
+            @php
+            function check($array, $key)
+            {
+                if (isset($array[$key])) {
+                    if (is_null($array[$key])) {
+                        return false;
+                    }
+                    return true;
+                }
+            }
+            @endphp
+            @foreach ($welfares as $index => $welfare)
+                    <tr>
 
+                            @switch($welfare->type)
+                            @case('S')
+                                @php ($text = 'บุคคล')
+                                @break
+                            @case('G')
+                                @php ($text = 'สันทนาการ')
+                                @break
+                            @endswitch
+
+
+                                <td class="text-center">{{ $welfare->title }}</td>
+                                @php ($sum = 0)
+                                @if (isset($year1[$index]))
+                                    <td class="text-end">{{ $year1[$index]->sum }}</td>
+                                    @php ($sum += $year1[$index]->sum)
+                                @else
+                                    <td class="text-end">0</td>
+                                @endif
+
+                                @if (isset($year2[$index]))
+                                    <td class="text-end">{{ $year2[$index]->sum }}</td>
+                                    @php ($sum += $year2[$index]->sum)
+                                @else
+                                    <td class="text-end">0</td>
+                                @endif
+
+                                @if (isset($year3[$index]))
+                                    <td class="text-end">{{ $year3[$index]->sum }}</td>
+                                    @php ($sum += $year3[$index]->sum)
+                                @else
+                                     <td class="text-end">0</td>
+                                @endif
+
+                                @if (isset($year4[$index]))
+                                    <td class="text-end">{{ $year4[$index]->sum }}</td>
+                                    @php ($sum += $year4[$index]->sum)
+                                @else
+                                    <td class="text-end">0</td>
+                                @endif
+                                @if (isset($year5[$index]))
+                                    <td class="text-end">{{ $year5[$index]->sum }}</td>
+                                    @php ($sum += $year5[$index]->sum)
+                                @else
+                                    <td class="text-end">0</td>
+                                @endif
+                                <td class="text-end">{{ $sum }}</td>
+
+                    </tr>
+                @endforeach
+        </tbody>
+     </table>
+     @endif
+    @if ($tb == 2)
+    <div class="row mt-3">
+        <div class="col-md-13">
+            <table class="table table-bordered align-items-center" >
+                <thead class="text-center text-light " id="bg" >
+                    <tr>
+                        <th rowspan="2" class="col-sm-1" valign="middle" align="center">ประเภทสวัสดิการ</th>
+                        <th colspan="12" class="col-sm-1">เดือน</th>
+                        <th rowspan="2" class="col-sm-2" valign="middle" align="center">จำนวนเงิน (บาท)</th>
+                    </tr>
+                    <tr class="text-center text-light " id="bg">
+                        <td class="col-sm-1">ม.ค.</td>
+                        <td class="col-sm-1">ก.พ.</td>
+                        <td class="col-sm-1">มี.ค.</td>
+                        <td class="col-sm-1">เม.ย.</td>
+                        <td class="col-sm-1">พ.ค.</td>
+                        <td class="col-sm-1">มิ.ย.</td>
+                        <td class="col-sm-1">ก.ค.</td>
+                        <td class="col-sm-1">ส.ค.</td>
+                        <td class="col-sm-1">ก.ย.</td>
+                        <td class="col-sm-1">ต.ค.</td>
+                        <td class="col-sm-1">พ.ย.</td>
+                        <td class="col-sm-1">ธ.ค.</td>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+    </div>
+    @endif
 </div>
